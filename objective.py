c4e20674df760036f447474addfb1f71121c994d
Keene Chin <Kaeladin@users.noreply.github.com>
2022-07-25 19:41:41 -0400

    Update objective.py

--- objective.py
+++ objective.py
@@ -3,41 +3,48 @@ import random
 import statistics
 
 
-class Room(object):
+
+class Room():
     def __init__(self,furniture,chargers,dirtiness):
         self.num_furniture = furniture
         self.dirtiness=dirtiness
         self.chargers=chargers
 
+class House():
+    def __init__(self, rooms, connections):
+        pass
 
-
-class Robot(object):
-    def __init__(self,width,height,charge):
+class Robot():
+    def __init__(self,width,height,charge, start_room):
         self.width=width
         self.height=height
         self.charge=charge
+        self.room = start_room
 
         
     def charge(self):
-        if room.chargers>0:
+        if self.room.chargers>0:
             self.charge=100
 
     def clean(self):
         if charge>0.2:
-            room.dirtiness=room.dirtiness-(room.dirtiness-0.05)
+            self.room.dirtiness=self.room.dirtiness-(self.room.dirtiness-0.05)
             charge=charge-0.1
-            if room.dirtiness>0.1:
-                room.clean
         else:
             robot.charge
+    
+    def move(self, room):
+        pass
 
-env = simpy.Environment()
+
+if __name__ == "__main__":
+    env = simpy.Environment()
 
 
-room1=Room(random.random(),random.randint(1,5),random.random())
+    room1=Room(random.random(),random.randint(1,5),random.random())
 
-room2=Room(random.random(),random.randint(1,5),random.random())
+    room2=Room(random.random(),random.randint(1,5),random.random())
 
-room3=Room(random.random(),random.randint(1,5),random.random())
+    room3=Room(random.random(),random.randint(1,5),random.random())
 
-robot=Robot(5,7,1)
+    robot=Robot(5,7,1, room1)
